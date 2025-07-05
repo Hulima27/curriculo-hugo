47067fa9a50107eb5ab4741ce4ad0589381e0397
Hulima27 <sabinohugo27@gmail.com>
2025-07-05 17:02:39 -0300

    Update curriculo.js

--- curriculo.js
+++ curriculo.js
@@ -1,7 +1,9 @@
 document.addEventListener("DOMContentLoaded", function () {
+    if (window.location.hash === "#curriculo") {
     fetch("Curriculo.html")
       .then(response => response.text())
       .then(data => {
         document.getElementById("mn-container").innerHTML = data;
       });
-    });
+    }
+});
